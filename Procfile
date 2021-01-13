b1ebd682974db32dc69f1d5e120216dd18925a44
Ohad <ohadohad5@gmail.com>
2021-01-13 13:24:37 +0200

    files update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python heroku_2_ohad.py
---
+++ flask_1_server_ohad.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import json
+import pickle
+from flask import Flask, request, jsonify, render_template
+import ctypes
+
+
+# print (ctypes.sizeof(ctypes.c_voidp))
+
+PKL_FILE_NAME = 'iris_rfc_pkl'
+
+app = Flask(__name__)
+model = pickle.load(open(PKL_FILE_NAME, 'rb'))
+
+@app.route('/test')
+def test():
+    """
+       test
+    """
+
+    return render_template('index5.html')
+
+@app.route('/predict_single')
+def predict2():
+    """
+       function for predicting a single row input by client requests as parameters of the URL
+       :return: single prediction label [0,1,2]
+       """
+    values = [np.array([request.args.get(x) for x in request.args])]
+    predict_request = model.predict(values)
+    return str(predict_request[0])
+
+
+@app.route('/predict_multiple', methods=["POST"])
+def results():
+    """
+       function for predicting a test dataset input as a body of the HTTP request
+       :return: prediction labels array
+       """
+    data = request.get_json(force=True)
+    data = pd.DataFrame(json.loads(data))
+    prediction = model.predict(data)
+    output = list(map(int, prediction))
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
